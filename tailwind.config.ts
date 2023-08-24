3fbdf4753d4ddf6d0a3c541615ef708d5ac9952e
Siddhant Madhur <siddhant.madhur@gmail.com>
2023-08-24 12:21:06 +0400

    added theme

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -3,7 +3,17 @@ import { type Config } from "tailwindcss";
 export default {
   content: ["./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        light: {
+          primary: "#4636c8",
+          secondary: "#d4d1eb",
+          accent: "#5348a8",
+          background: "#f2f1f9",
+          text: "#07060e",
+        },
+      },
+    },
   },
   plugins: [],
 } satisfies Config;
