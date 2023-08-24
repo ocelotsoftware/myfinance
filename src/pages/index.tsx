108456955c37e99368ecef7b6e151187ea68496d
Siddhant Madhur <siddhant.madhur@gmail.com>
2023-08-24 12:14:02 +0400

    get user serverside

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,8 +2,19 @@ import { signIn, signOut, useSession } from "next-auth/react";
 import Head from "next/head";
 import Link from "next/link";
 import { api } from "@/utils/api";
+import { GetServerSideProps } from "next";
+import { getServerAuthSession } from "@/server/auth";
+
+export const getServerSideProps: GetServerSideProps = async (context) => {
+  const session = await getServerAuthSession(context);
+  return {
+    props: { session },
+  };
+};
 
 export default function Home() {
+  const { data } = useSession();
+
   return (
     <>
       <Head>
@@ -14,10 +25,13 @@ export default function Home() {
         />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main>
-        hi
+      <main className="flex h-screen w-full place-items-center justify-center">
+        {data ? (
+          <button onClick={() => void signOut()}>sign out</button>
+        ) : (
+          <button onClick={() => void signIn()}>sign in</button>
+        )}
       </main>
     </>
   );
 }
-
